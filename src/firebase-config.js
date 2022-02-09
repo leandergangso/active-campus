2202acd86909fc2b7934494d7daf37cf0e5b25ad
Leander Gangsø <Leander.gangso98@gmail.com>
2022-02-09 17:51:26 +0100

    move values to env file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIiCDsPf_znYeG9o3WHh3ZJgXvsUfUvUk",
-  authDomain: "usnaktivcampus.firebaseapp.com",
-  projectId: "usnaktivcampus",
-  storageBucket: "usnaktivcampus.appspot.com",
-  messagingSenderId: "312134547172",
-  appId: "1:312134547172:web:c5e900139156d86cf0def1"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
