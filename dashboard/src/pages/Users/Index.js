c2962b257b9c97d5448cfe3b46a23dd035d89a76
Leander Gangsø <Leander.gangso98@gmail.com>
2022-02-17 16:18:42 +0100

    wip user role page

--- dashboard/src/pages/Users/Index.js
+++ dashboard/src/pages/Users/Index.js
@@ -1,6 +1,63 @@
+import Input from "../../components/Actions/Input"
+import Button from "../../components/Actions/Button"
+import Dropdown from "../../components/Actions/Dropdown"
+import UserRoleList from "./components/UserRoleList"
 
 const Index = () => {
-  return <div></div>;
-};
+  const userList = [
+    { name: 'Brukerens navn', email: 'bruker@epost.no', role: 'Administrator' },
+    { name: 'Brukerens navn', email: 'bruker@epost.no', role: 'Administrator' },
+    { name: 'Brukerens navn', email: 'bruker@epost.no', role: 'Administrator' },
+    { name: 'Brukerens navn', email: 'bruker@epost.no', role: 'Moderator' },
+  ]
 
-export default Index;
+  const roles = ['Administrator', 'Moderator', 'Hjelper']
+
+  return (
+    <div className="flex gap-14">
+      <div className="flex flex-col gap-5">
+        <h1 className="font-bold text-2xl">Alle brukere</h1>
+
+        <div>
+          <UserRoleList users={userList} />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 mb-10">
+          <h1 className="font-bold text-2xl">Legg til bruker</h1>
+
+          <div>
+            <label htmlFor="">Epost</label>
+            <Input placeholder='brukers@epost.no' />
+          </div>
+          <div>
+            <label htmlFor="">Rolle</label>
+            <Dropdown options={roles} />
+          </div>
+
+          <Button>Legg til</Button>
+        </div>
+        
+        <div className="flex flex-col gap-5">
+          <h1 className="font-bold text-2xl">Endre bruker rolle</h1>
+
+          <div className="flex flex-col gap-5">
+            <div>
+              <label htmlFor="">Epost</label>
+              <Input placeholder='brukers@epost.no' />
+            </div>
+            <div>
+              <label htmlFor="">Rolle</label>
+              <Dropdown options={roles} />
+            </div>
+
+            <Button>Oppdater</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Index
---
+++ dashboard/src/pages/Users/components/UserRoleList.js
@@ -0,0 +1,10 @@
+
+const UserRoleList = () => {
+  return (
+    <div>
+      user role list
+    </div>
+  )
+}
+
+export default UserRoleList
